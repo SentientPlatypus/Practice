ad17b474e4347887828731c34881cce2e7528906
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-09-29 23:18:47 -0400

    started beads

---
+++ cpp/beads.cpp
@@ -0,0 +1,25 @@
+/*
+ID: geneust1
+TASK: beads
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int main() {
+    cout << "WE OUT" << endl;
+
+    ofstream fout ("beads.out");
+    ifstream fin ("beads.in");
+
+    int len;
+    fin >> len;
+
+    const int necklen = len;
+    char necklace[4];
+
+}
