6575ca8af987580e7268dd010f1c692a2ffea535
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-11-26 20:00:10 -0500

    updated code

--- py/area_of_hand.py
+++ py/area_of_hand.py
@@ -2,11 +2,17 @@ import cv2
 import numpy as np
 
 
+class Paper:
+    A4 = 11.59 * 28
+    LETTER = 21.59 * 27.94
+
+
 
 class AreaCalculator:
-    def __init__(self, hand_image:np.ndarray, area_of_paper:float) -> None:
-        self.hand_image = cv2.resize(hand_image, )
+    def __init__(self, name, hand_image:np.ndarray, area_of_paper:float) -> None:
+        self.hand_image = hand_image
         self.area_of_paper = area_of_paper
+        self.name = name
 
         self.__hsv_threshold_hue = [0.0, 180.0]
         self.__hsv_threshold_saturation = [0.0, 255.0]
@@ -59,12 +65,14 @@ class AreaCalculator:
         x_hand, y_hand, w_hand, h_hand = cv2.boundingRect(hand_contour)
 
         # Write the area in the top-left corner of the bounding box
-        cv2.putText(self.hand_image, f"Paper Area: {paper_area:.2f} pixels", (x_paper, y_paper), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
-        cv2.putText(self.hand_image, f"Hand Area: {hand_area:.2f} pixels", (x_hand, y_hand), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+        cv2.putText(self.hand_image, f"Paper Area: {paper_area:.2f} pixels -> {self.area_of_paper}cm^2", (x_paper, y_paper), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 0), 2)
+        cv2.putText(self.hand_image, f"Hand Area: {hand_area:.2f} pixels -> {hand_area * (self.area_of_paper / paper_area) :.3f}cm^2", (x_hand, y_hand), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 0), 2)
 
-        print(f"Paper Area: {paper_area:.2f}, hand area: {hand_area:.2f}")
+        print(f"Paper Area (pixels): {paper_area:.2f}, hand area (pixels): {hand_area:.2f}")
+        print(f"Paper Area (cm3): {self.area_of_paper}, hand area (cm3): {hand_area * (self.area_of_paper / paper_area) :.3f}")
         # Display the image with the detected contours and areas
         cv2.imshow("Detected Paper and Hand", self.hand_image)
+        cv2.imwrite(f"res/{self.name}.png", self.hand_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -72,6 +80,6 @@ class AreaCalculator:
 
 
 if __name__ == "__main__":
-    handimage = cv2.imread("genedarkhand.JPG")
-    areacalculator = AreaCalculator(handimage, area_of_paper= 21.59 * 28)
+    # areacalculator = AreaCalculator("JUN",  cv2.imread("jundarkhand2.JPG"), Paper.LETTER)
+    areacalculator = AreaCalculator("GENE",  cv2.imread("genedarkhand.JPG"), Paper.A4)
     areacalculator.calculate_area()
