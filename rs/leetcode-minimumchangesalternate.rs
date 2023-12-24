31c17a057dcb791a27a4ca951cb944be319b6144
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-12-23 22:46:29 -0500

    daily solution rs

--- rs/leetcode-minimumchangesalternate.rs
+++ rs/leetcode-minimumchangesalternate.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn min_operations(s: String) -> i32 {
         let mut changes_01 = 0;
-        let mut changes_10 = 1;
+        let mut changes_10 = 0;
 
         for (i, c) in s.chars().enumerate() {
             if i % 2 == 0 && c != '0' {
