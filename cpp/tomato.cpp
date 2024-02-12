87201b41e6434e85c3acf1a937b6bc7902d59660
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-02-12 14:20:10 -0500

    daily solution py

---
+++ cpp/tomato.cpp
@@ -0,0 +1,21 @@
+#include <queue>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+int main() {
+    int N, M;
+
+    unordered_map<int, vector<int>> map;
+
+    for (int i = 0; i < N; i ++) {
+        for (int j = 0; j < M; j++) {
+            int k;
+            cin >> k;
+            if (map[k]) {
+                
+            }
+        }
+    }
+}
---
+++ py/tomato.py
@@ -0,0 +1,60 @@
+def neighbors(row, col, box) -> list[int]:
+	return [[r, c] for r, c in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)] if r >= 0 and r < len(box) and c >= 0 and c < len(box[0]) and box[r][c] == 0]
+
+def newmin(box):
+	M, N = len(box), len(box[0])
+	q = []
+	visited = []
+	for i in range(M):
+		for j in range(N):
+			if box[i][j] == 1:
+				q.append([i, j])
+	
+	days = 0
+	while True:
+		toappend = []
+		changed = False
+		while q:
+			current = q.pop(0)
+			visited.append(current)
+
+			r, c = current
+			changed = True
+			box[r][c] = 1
+			
+			neighborhood = neighbors(r, c, box)
+			for neighbor in neighborhood:
+				if neighbor not in visited:
+					toappend.append(neighbor)
+		days += 1
+		if not changed:
+			return -1
+			
+		all_ripe = True
+		for i in range(M):
+			for j in range(N):
+				if box[i][j] == 0:
+					all_ripe = False
+					break
+		if all_ripe:
+			return days - 1
+		
+		for cell in toappend:
+			q.append(cell)
+
+
+def read_box():
+	M, N = list(map(int, input().split()))
+	box = []
+	for i in range(N):
+		row = list(map(int, input().split()))
+		box.append(row)
+	return box
+
+def print_box(box):
+	for row in box:
+		print(*row, sep=" ")
+
+box = read_box()
+days = newmin(box)
+print(days)
