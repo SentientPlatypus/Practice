a146c9c303adb08449790a6607956f6b256bfd4b
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-04-12 20:57:09 -0400

    floyds tortoise and hare

--- py/findDuplicate.py
+++ py/findDuplicate.py
@@ -1,9 +1,24 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        m = {}
-        for n in nums:
-            if n  in m.keys():
-                return n
-            else:
-                m[n] = 1
-        return m[-1]
+        s1 = 0
+        s2 = 0
+        f = 0
+
+        while 1:
+            s1 = nums[s1] #traverse once
+            f = nums[nums[f]] #traverse twice
+
+            if s1 == f:
+                break
+        
+        while 1:
+            s1 = nums[s1]
+            s2 = nums[s2]
+            if s1 == s2:
+                return s1
+        
+        return 1
+        
+        
+        
+
