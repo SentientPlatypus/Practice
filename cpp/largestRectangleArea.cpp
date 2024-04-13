0b4c14671019ad2f8ab1b79a1aa4899b57f04fb6
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-04-13 15:24:14 -0400

    hist!

--- cpp/largestRectangleArea.cpp
+++ cpp/largestRectangleArea.cpp
@@ -1,27 +1,62 @@
 #include <vector>
 #include <stack>
-#include <algorithm>
-
+#include <iostream>
 using namespace std;
 
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> s;
-        int maxArea = 0;
-        
-        for (int i = 0; i <= n; ++i) {
-            while (!s.empty() && (i == n || heights[s.top()] >= heights[i])) {
-                int h = (i == n) ? 0 : heights[i];
-                int index = s.top();
-                s.pop();
-                int width = s.empty() ? i : i - s.top() - 1;
-                maxArea = max(maxArea, heights[index] * width);
+
+    vector<int> getLeftLimits(vector<int>& heights) {
+        stack<int> st;
+        vector<int> v(heights.size(), 0);
+        st.push(0);
+        v.push_back(0);
+
+        for (int i = 1; i < heights.size(); i++) {
+            
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
             }
-            s.push(i);
+            if (st.empty()) {
+                v[i] = 0;
+                st.push(i);
+            }
+            else v[i] = st.top() + 1;
+            st.push(i);
         }
-        
-        return maxArea;
+        return v;
+    }
+
+    vector<int> getRightLimits(vector<int>& heights) {
+        stack<int> st;
+        vector<int> v(heights.size(), 0);
+
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                v[i] = heights.size() - 1;
+            } else {
+                v[i] = st.top() - 1;
+            }
+            st.push(i);
+        }
+        return v;
+    }
+
+
+    int largestRectangleArea(vector<int>& heights) {
+        int m = 0;
+        vector<int> l = getLeftLimits(heights);
+        vector<int> r = getRightLimits(heights);
+
+        for (int i : v) {cout << i;}
+        cout << endl;
+
+        for (int i = 0; i < heights.size(); i ++) {
+            m = max(heights[i] * (r[i] - l[i] + 1), m);
+        }        
+        return m;
     }
 };
