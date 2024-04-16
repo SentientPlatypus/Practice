c9cb5209d4434cd51d660cd2afaaaf98c2f1492e
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-04-16 12:07:37 -0400

    cheese

---
+++ cpp/vpsstring.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftCount = 0, rightCount = 0;
+        
+        for (char c : s) {
+            leftCount += c == '(' ? 1 : -1;
+            rightCount += c == ')' ? -1 : 1;
+            
+            if (rightCount < 0) {
+                break;
+            }
+            
+            leftCount = max(leftCount, 0);
+        }
+
+        return leftCount == 0;
+    }
+};
