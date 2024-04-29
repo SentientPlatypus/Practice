e489bcbe02e95a362de9e596ccdab7a002b7879b
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-04-29 10:15:37 -0400

    daily solution cpp

---
+++ cpp/minimumAddedCoins.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        //[1, 4, 10], target = 19. X is obtainable if there is a subsequence of coins that adds to target.
+        sort(coins.begin(), coins.end());
+        
+        int m = 1;
+        int added = 0;
+        int i = 0;
+
+        while (m <= target) {
+            if (i < coins.size() && coins[i] <= m) m += coins[i ++];
+            else {
+                m += m;
+                added ++;
+            }
+        }
+        return added;
+    }
+};
