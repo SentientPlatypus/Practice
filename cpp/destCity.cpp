b026b0fe34a8a235b4b0b527b5d01ac6884e7abf
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-05-11 21:44:01 -0400

    daily solution cpp

---
+++ cpp/destCity.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> cities;
+        for (const auto& path : paths) {
+            cities.insert(path[0]);
+        }
+
+        for (const auto& path : paths) {
+            const std::string& dest = path[1];
+            if (cities.find(dest) == cities.end()) {
+                return dest;
+            }
+        }
+
+        return "";
+    }
+};
