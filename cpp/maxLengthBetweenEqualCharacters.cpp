2c31839c3d6c0de47f81453ce5d965b7a1140a01
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-05-21 22:36:10 -0400

    daily solution, n

---
+++ cpp/maxLengthBetweenEqualCharacters.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int l = 0;
+        int r = s.length();
+        
+    }
+};
