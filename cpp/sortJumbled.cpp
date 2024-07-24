409ff0debe7d7cf6d4bc065f238ae22b689cd55c
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-07-24 10:01:28 -0400

    daily solution cpp

---
+++ cpp/sortJumbled.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        vector<pair<int, int>> storePairs;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            string number = to_string(nums[i]);
+            string formed = "";
+            for (int j = 0; j < number.size(); ++j) {
+                formed = formed + (to_string(mapping[number[j] - '0']));
+            }
+            int mappedValue = stoi(formed);
+            storePairs.push_back({mappedValue, i});
+        }
+
+
+        sort(storePairs.begin(), storePairs.end());
+        vector<int> answer;
+        for (auto pair : storePairs) {
+            answer.push_back(nums[pair.second]);
+        }
+        return answer;
+    }
+};
