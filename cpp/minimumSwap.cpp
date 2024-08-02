59e0bf670639f7f268d29b0737482754f857d381
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-08-02 09:54:54 -0400

    daily solution cpp;

---
+++ cpp/minimumSwap.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        unordered_map<string, int> m1;
+        unordered_map<string, int> m2;
+
+        if (s1.length() != s2.length()) {
+            return -1;
+        }
+
+        for (int i =0; i < s1.length(); i++) {
+            
+        }
+
+        
+    }
+};
--- cpp/minswaps.cpp
+++ cpp/minswaps.cpp
@@ -1,23 +1,38 @@
-#include <string>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 
 class Solution {
 public:
-    int minimumSwap(string s1, string s2) {
-        unordered_map<string, int> m1;
-        unordered_map<string, int> m2;
-
-        if (s1.length() != s2.length()) {
-            return -1;
-        }
-
-        for (int i =0; i < s1.length(); i++) {
-            
-        }
-
+    int minSwaps(vector<int>& nums) {
+        int totalOnes = 0;
+        int n = nums.size();
         
+        for (int i : nums) totalOnes += i == 1;
+
+        vector<int> extendedNums = nums;
+        extendedNums.insert(extendedNums.end(), nums.begin(), nums.end());
+
+        int l = 0;
+        int r = totalOnes - 1;
+        int minSwaps = __INT_MAX__;
+        int swaps = 0;
+
+        for (int i = l; i <= r; i++) if (extendedNums[i] == 0) swaps++;
+            
+        
+        minSwaps = min(minSwaps, swaps);
+
+
+        while (r < extendedNums.size() - 1) {
+            if (extendedNums[l] == 0) swaps--;
+            l++;
+            r++;
+            if (extendedNums[r] == 0) swaps++;
+            minSwaps = min(minSwaps, swaps);
+        }
+
+        return minSwaps;
     }
 };
