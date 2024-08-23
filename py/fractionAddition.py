7e4702ac8cdedef8571fe150c3572f1b370f56a9
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-08-23 10:27:49 -0400

    daily solution py

---
+++ py/fractionAddition.py
@@ -0,0 +1,22 @@
+import re
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        nums = list(map(int, re.findall(r'[+-]?\d+', expression)))
+        print(nums)
+        n = 0
+        d = 1
+
+
+        for i in range(0, len(nums), 2):
+            num, den = nums[i], nums[i + 1]
+            n = n * den + num * d
+            d *= den
+        
+        common_divisor = gcd(n, d)
+        return f"{n // common_divisor}/{d // common_divisor}"
+    
+    def gcd(a, b):
+        if b == 0:
+            return a
+        return Solution.gcd(b, a % b)
