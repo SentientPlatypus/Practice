a29f4d7c3a847c69e2435a0170b482c2ecc346c1
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2025-07-09 23:57:30 -0400

    daily lc

---
+++ 594.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            if (nums[r] - nums[l] == 1) {
+                ans = max(ans, r - l + 1);
+                r++;
+            } else if (nums[r] - nums[l] > 1) {
+                l++;
+            } else {
+                r++;
+            }
+        }
+        return ans;
+    }
+};
