fc2847f2eef2ca7c8d65c9dc12e209f9659fefcf
Geneustace Wicaksono <gjw62@cornell.edu>
2026-03-26 23:13:45 -0400

    prac

---
+++ py/33.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                #the left half is sorted
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    # target is in the unsorted right hand side.
+                    l = mid + 1
+
+            else:
+                #right hand side is sorted
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    #target is in unsorted left half
+                    r = mid - 1
+        return -1
