c039b14cd050d4168acf87cea646779186d16358
Geneustace Wicaksono <gjw62@cornell.edu>
2026-04-22 11:20:55 -0400

    110.py

---
+++ py/110.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.balanced = True
+        self.depth(root)
+        return self.balanced
+    
+
+    def depth(self, root):
+        if not root:
+            return 0
+        
+        ldepth = self.depth(root.left)
+        rdepth = self.depth(root.right)
+
+        if abs(ldepth - rdepth) > 1:
+            self.balanced = False
+        
+        return 1 + max(ldepth, rdepth)
