911748ce2db4c586da4911cc108d64e35fd4f606
Geneustace Wicaksono <gjw62@cornell.edu>
2026-04-25 12:09:18 -0400

    235

--- py/235.py
+++ py/235.py
@@ -1,37 +1,24 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-class Solution:
-    def contains(self, root: TreeNode, target: TreeNode):
-        if root.val == target.val:
-            return True
+class Solution: 
         
-        inLeft = False
-        inRight = False
-        if root.left:
-            inLeft = self.contains(root.left, target)
-        if root.right:
-            inRight = self.contains(root.right, target)
-        
-        return inLeft or inRight
-
-
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        inRoot = self.contains(root, p) and self.contains(root, q)
-        inLeft = root.left and self.contains(root.left, p) and self.contains(root.left, q)
-        inRight = root.right and self.contains(root.right, p) and self.contains(root.right, q)
-        if inRoot:
-            if inLeft:
-                return self.lowestCommonAncestor(root.left, p, q)
-            elif inRight:
-                return self.lowestCommonAncestor(root.right, p, q)
-            else:
-                return root
-        return None
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
+        #same node cannot exist in both left and right, so if left and right return true, then root is a common ancestor.
+        if left and right:
+            return root
+        return left or right
+
+            
 
 
