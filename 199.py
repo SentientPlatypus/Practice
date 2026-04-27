8e1b4e4fb35696d8d2eee1e85ada18a2513d90ab
Geneustace Wicaksono <gjw62@cornell.edu>
2026-04-27 13:20:01 -0400

    199

---
+++ 199.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelorder(self, root:Optional[TreeNode]):
+        res = []
+
+        q = deque()
+        q.append((root, 0))
+
+        while q:
+            cur, dpth = q.popleft()
+
+            if cur:
+                if dpth < len(res):
+                    res[dpth] = cur.val
+                else:
+                    res.append(cur.val)
+                
+                q.append((cur.left, dpth + 1))
+                q.append((cur.right, dpth + 1))
+        
+        return res
+
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        levelorder = self.levelorder(root)
+        return levelorder
