f7f80da5e60f21be20e169ea0513abccb39c3478
Geneustace Wicaksono <gjw62@cornell.edu>
2026-04-30 23:50:48 -0400

    optimized 230

--- py/230.py
+++ py/230.py
@@ -15,3 +15,21 @@ class Solution:
         self.inOrder(root)
         print(self.trav)
         return self.trav[k - 1]
+
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stk = []
+        stk.append(root)
+        cur = root
+        while stk or cur:
+            while cur:
+                stk.append(cur)
+                cur = cur.left
+        
+            cur = stk.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
